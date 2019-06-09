66767e9bab21290e038dbcec5258b39ca08a0da8
jym <jymdxl@gmail.com>
2019-06-09 08:34:51 +0800

    first bug

---
+++ app/src/main/java/com/jym/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,50 @@
+package com.jym.criminalintent;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.List;
+import java.util.UUID;
+
+public class CrimePagerActivity extends AppCompatActivity {
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
+    private static final String EXTRA_CRIME_ID = "com.jym.criminalIntent_id";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+        UUID crimeId=(UUID)getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+        mCrimes = CrimeLab.get(this).getCrimes();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+
+    }
+
+    public static Intent newIntent(Context packagecontext, UUID crimeId) {
+        Intent intent = new Intent(packagecontext, CrimeActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
+
+}
